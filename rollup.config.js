2ebc94a05b889011886a452e0c218ad79d2b1ff3
Hanrea <hanrea@qq.com>
2024-08-24 20:23:48 +0800

    ✨ update v0.1.2

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,21 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
+/**
+ * @Author: Hanrea
+ * @Date: 2023/9/11 17:36:53
+ * @LastEditors: Hanrea
+ * @LastEditTime: 2023/9/11 17:36:53
+ * Description:
+ * Copyright: Copyright (©)}) 2023 Vecmat.com. All rights reserved.
+ */
+
 import { summary } from 'rollup-plugin-summary';
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 /**
  * @type {import('rollup').RollupOptions}
  */
-const configs = {
-  input: ['./src/index.ts'],
+export default {
+  input: ['src/index.ts'],
   output: [
     {
       dir: 'libs',
@@ -15,7 +24,7 @@ const configs = {
       interop: 'auto',
       exports: 'named',
       preserveModules: true,
-      entryFileNames: '[name].cjs',
+      entryFileNames: 'cjs/[name].cjs'
     },
     {
       dir: 'libs',
@@ -24,11 +33,11 @@ const configs = {
       interop: 'auto',
       exports: 'named',
       preserveModules: true,
-      entryFileNames: '[name].mjs'
+      entryFileNames: 'esm/[name].mjs'
     }
   ],
-  plugins: [typescript({ tsconfig: 'tsconfig.json' }), nodeResolve(), summary()],
+  plugins: [typescript({ tsconfig: 'tsconfig.build.json' }), nodeResolve(), summary()],
   external: [/node_modules/],
 };
 
-export default configs;
+
